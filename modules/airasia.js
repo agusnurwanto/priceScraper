57dc4e94ca68bd083872240cf172479f5798de5e
Muhammad Abdul Jabbaar <raabbajam@gmail.com>
2014-12-05 13:51:43 +0700

    aa  now use one request, 101

--- modules/airasia.js
+++ modules/airasia.js
@@ -1,38 +1,55 @@
-var Base = require('../base');
+/*jshint -W079 */
+var Base    = require('../base');
 var Promise = require('promise');
+var _       = require('lodash');
+function init(args) {
+	this._super(args);
+	this.defaultModes = ['101'];
+	// this.parallel = true;
+}
+function  getAll() {
+	return this._super()
+	.then(function (results) {
+		// console.log(results);
+		var bodies = results.map(function (res) {
+			return JSON.parse(res.body)[0];
+		})
+		// console.log(bodies,'bodies');
+		return Promise.resolve(bodies);
+	})
+	.catch(function (err) {
+		return Promise.reject(err);
+	})
+}
+function calculateAdult(results) {
+	var _101  = results[0];
+	var basic = +_101.depart.fare.adults.replace('1 x ', '');
+	var taxes = _.values(_101.depart.taxesAndFees);
+	var tax   = taxes.reduce(function(all, _tax) {
+		return all + _tax;
+	}, 0);
+	return basic + tax;
+}
+function calculateChild(results) {
+	var _101  = results[0];
+	var basic = +_101.depart.fare.adults.replace('1 x ', '');
+	return basic + tax;
+}
+function calculateInfant(results) {
+	var _101 = results[0];
+	return +_101.depart.fare.infants.replace('1 x ', '');
+}
+function calculateBasic(results) {
+	var _101 = results[0];
+	return +_101.depart.fare.adults.replace('1 x ', '');
+}
 var airasiaPrototype = {
-	getAll: function () {
-		return this._super()
-			.then(function (results) {
-				// console.log(results);
-				var bodies = results.map(function (res) {
-					return JSON.parse(res.body)[0];
-				})
-				// console.log(bodies,'bodies');
-				return Promise.resolve(bodies);
-			})
-			.catch(function (err) {
-				return Promise.reject(err);
-			})
-	},
-	calculateAdult: function (results) {
-		var _100 = results[0];
-		return +_100.totalIDR;
-	},
-	calculateChild: function (results) {
-		var _100 = results[0];
-		var _110 = results[1];
-		return _110.totalIDR - _100.totalIDR;
-	},
-	calculateInfant: function (results) {
-		var _100 = results[0];
-		var _101 = results[2];
-		return _101.totalIDR - _100.totalIDR;
-	},
-	calculateBasic: function (results) {
-		var _100 = results[0];
-		return +_100.depart.fare.adults.replace('1 x ', '');
-	}
+	init            : init,
+	getAll          : getAll,
+	calculateAdult  : calculateAdult,
+	calculateChild  : calculateChild,
+	calculateInfant : calculateInfant,
+	calculateBasic  : calculateBasic,
 };
-var Garuda = Base.extend(airasiaPrototype);
-module.exports = Garuda;
+var Airasia    = Base.extend(airasiaPrototype);
+module.exports = Airasia;
