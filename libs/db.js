81537b7e79ad6931472a4dc375447fd4dd544a5a
Muhammad Abdul Jabbaar <raabbajam@gmail.com>
2015-01-24 16:39:11 +0700

    adding flex host;

--- libs/db.js
+++ libs/db.js
@@ -3,4 +3,8 @@ var elasticSearchClient = new ElasticSearchClient({
     host: 'folbek.me',
     port: 9200
 });
+if (process.env.CONFIG === 'local')
+    config.host = 'localhost';
+if (!!process.env.SCRAPE_HOST)
+    config.host = process.env.SCRAPE_HOST;
 module.exports = elasticSearchClient;
